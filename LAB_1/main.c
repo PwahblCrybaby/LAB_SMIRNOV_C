10fb0329fc86dd0d357ca1884b9223240f541f5a
PwahblCrybaby <147195286+PwahblCrybaby@users.noreply.github.com>
2023-10-16 23:58:54 +0300

    Update main.c

--- LAB_1/main.c
+++ LAB_1/main.c
@@ -1,54 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <pthread.h>
+#include <time.h>
 
 #define N 512
 #define M 512
 
-int matrix[N][M];
+int A[N][M];
+
+struct Args {
+    int col_begin;
+    int col_end;
+    int* max;
+};
 
 void *find_max(void *arg) {
-    int *col = (int *) arg;
-    int max = matrix[0][*col];
-    for (int i = 1; i < N; i++) {
-        if (matrix[i][*col] > max) {
-            max = matrix[i][*col];
+    struct Args args = *(struct Args*) arg;
+    //printf("Start %d\n", args.col_begin);
+    for (int j = args.col_begin; j < args.col_end; ++j) {
+        args.max[j] = A[0][j];
+        for (int i = 0; i < N; ++i) {
+            if (A[i][j] > args.max[j]) {
+                args.max[j] = A[i][j];
+            }
         }
     }
-    return (void *) max;
+    //printf("End %d\n", args.col_end);
+    return NULL;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     // Заполнение матрицы случайными числами
-    srand(time(NULL));
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            matrix[i][j] = rand() % 100;
+            A[i][j] = rand();
         }
     }
 
-    // Создание массива потоков
-    pthread_t threads[M];
+    // Поиск максимальных значений в столбцах элементов матрицы 𝐴
+    int max[M];
+    int num_threads = atoi(argv[1]);
+    printf("Threads: %d\n", num_threads);
 
-    // Запуск потоков
-    for (int i = 0; i < M; i++) {
-        pthread_create(&threads[i], NULL, find_max, (void *) &i);
+    pthread_t threads[num_threads];
+    struct Args args[num_threads];
+    for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+        args[thread_id].col_begin = M / num_threads * thread_id;
+        args[thread_id].col_end = M / num_threads * (thread_id + 1);
+        //printf("%d: %d - %d\n", thread_id, args[thread_id].col_begin, args[thread_id].col_end);
+        args[thread_id].max = max;
     }
-
-    // Получение результатов
-    int max_values[M];
-    for (int i = 0; i < M; i++) {
-        void *result;
-        pthread_join(threads[i], &result);
-        max_values[i] = (int) result;
+    args[num_threads - 1].col_end = M;
+    clock_t start_time = time(NULL);
+    for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+        pthread_create(&threads[thread_id], NULL, find_max, &args[thread_id]);
     }
+    for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+        pthread_join(threads[thread_id], NULL);
+    }
+    clock_t end_time = time(NULL);
 
     // Вывод результатов
-    for (int i = 0; i < M; i++) {
-        printf("%d ", max_values[i]);
+    printf("Максимальные значения в столбцах элементов матрицы 𝐴:\n");
+    for (int j = 0; j < M; j++) {
+        //printf("%d\n", max[j]);
     }
     printf("\n");
+    //double time = (end_time - start_time);
+    printf("Время выполнения программы: %ld секунд\n", (end_time - start_time));
 
     return 0;
 }
